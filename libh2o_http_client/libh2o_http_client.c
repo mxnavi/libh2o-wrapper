7f4ef149be5dc8ec230da59746a7d072e019d2db
Baodong Chen <chenbaodong@mxnavi.com>
2019-01-29 15:42:28 +0800

    fix debug logs for android

--- libh2o_http_client/libh2o_http_client.c
+++ libh2o_http_client/libh2o_http_client.c
@@ -8,6 +8,12 @@
 *   MEMO        :
 *****************************************************************************/
 
+#ifndef LOG_TAG
+#define LOG_TAG "libh2o.http"
+#endif
+
+// #define LOG_NDEBUG 0
+
 /****************************************************************************
 *                       Include File Section                                *
 *****************************************************************************/
@@ -16,11 +22,29 @@
 #include <stdlib.h>
 #include "h2o.h"
 
+#include "libh2o_http_client.h"
+
+#ifdef PLATFORM_SDK_VERSION
+#include <cutils/log.h>
+#elif defined(__ANDROID__)
+#include <android/log.h>
+#define ALOGV(fmt, args...)                                                    \
+    __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, fmt, ##args)
+#define ALOGD(fmt, args...)                                                    \
+    __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, fmt, ##args)
+#define ALOGI(fmt, args...)                                                    \
+    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, fmt, ##args)
+#define ALOGW(fmt, args...)                                                    \
+    __android_log_print(ANDROID_LOG_WARN, LOG_TAG, fmt, ##args)
+#define ALOGE(fmt, args...)                                                    \
+    __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, fmt, ##args)
+#else
+#endif
+
 #ifndef MIN
 #define MIN(a, b) (((a) > (b)) ? (b) : (a))
 #endif
 
-#include "libh2o_http_client.h"
 
 /****************************************************************************
 *                       Macro Definition Section                            *
@@ -30,11 +54,35 @@
 #define NOTIFICATION_CONN 0
 #define NOTIFICATION_QUIT 0xFFFFFFFF
 
+#ifdef ALOGV
+#define LOGV ALOGV
+#else
 #define LOGV(fmt, args...) ((void)fprintf(stderr, fmt "\n", ##args))
+#endif
+
+#ifdef ALOGD
+#define LOGD ALOGD
+#else
 #define LOGD(fmt, args...) ((void)fprintf(stderr, fmt "\n", ##args))
+#endif
+
+#ifdef ALOGI
+#define LOGI ALOGI
+#else
 #define LOGI(fmt, args...) ((void)fprintf(stderr, fmt "\n", ##args))
+#endif
+
+#ifdef ALOGW
+#define LOGW ALOGW
+#else
 #define LOGW(fmt, args...) ((void)fprintf(stderr, fmt "\n", ##args))
+#endif
+
+#ifdef ALOGE
+#define LOGE ALOGE
+#else
 #define LOGE(fmt, args...) ((void)fprintf(stderr, fmt "\n", ##args))
+#endif
 
 #ifndef ASSERT
 #define ASSERT assert
--- libh2o_websocket_client/libh2o_websocket_client.c
+++ libh2o_websocket_client/libh2o_websocket_client.c
@@ -8,6 +8,11 @@
 *   MEMO        :
 *****************************************************************************/
 
+#ifndef LOG_TAG
+#define LOG_TAG "libh2o.websocket"
+#endif
+
+// #define LOG_NDEBUG 0
 /****************************************************************************
 *                       Include File Section                                *
 *****************************************************************************/
@@ -22,6 +27,22 @@
 
 #include "libh2o_websocket_client.h"
 
+#ifdef PLATFORM_SDK_VERSION
+#include <cutils/log.h>
+#elif defined(__ANDROID__)
+#include <android/log.h>
+#define ALOGV(fmt, args...)                                                    \
+    __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, fmt, ##args)
+#define ALOGD(fmt, args...)                                                    \
+    __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, fmt, ##args)
+#define ALOGI(fmt, args...)                                                    \
+    __android_log_print(ANDROID_LOG_INFO, LOG_TAG, fmt, ##args)
+#define ALOGW(fmt, args...)                                                    \
+    __android_log_print(ANDROID_LOG_WARN, LOG_TAG, fmt, ##args)
+#define ALOGE(fmt, args...)                                                    \
+    __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, fmt, ##args)
+#else
+#endif
 /****************************************************************************
 *                       Macro Definition Section                            *
 *****************************************************************************/
@@ -34,11 +55,36 @@
 
 #define DISPOSE_TIMEOUT_MS 1000
 
+#ifdef ALOGV
+#define LOGV ALOGV
+#else
 #define LOGV(fmt, args...) ((void)fprintf(stderr, fmt "\n", ##args))
+#endif
+
+#ifdef ALOGD
+#define LOGD ALOGD
+#else
 #define LOGD(fmt, args...) ((void)fprintf(stderr, fmt "\n", ##args))
+#endif
+
+#ifdef ALOGI
+#define LOGI ALOGI
+#else
 #define LOGI(fmt, args...) ((void)fprintf(stderr, fmt "\n", ##args))
+#endif
+
+#ifdef ALOGW
+#define LOGW ALOGW
+#else
 #define LOGW(fmt, args...) ((void)fprintf(stderr, fmt "\n", ##args))
+#endif
+
+#ifdef ALOGE
+#define LOGE ALOGE
+#else
 #define LOGE(fmt, args...) ((void)fprintf(stderr, fmt "\n", ##args))
+#endif
+
 
 #ifndef ASSERT
 #define ASSERT assert
