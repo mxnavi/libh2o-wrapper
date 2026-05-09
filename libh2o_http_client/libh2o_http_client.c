d51f3fc53797a575f517cff0874034de2361949b
Baodong Chen <chenbaodong@mxnavi.com>
2026-05-09 09:40:59 +0800

    http: enlarge default 'max_buffer_size' for http client to 65536

--- libh2o_http_client/libh2o_http_client.c
+++ libh2o_http_client/libh2o_http_client.c
@@ -890,7 +890,7 @@ libh2o_http_client_start(const struct http_client_init_t *client_init)
         c->ctx.keepalive_timeout = client_init->timeout > 1000
                                        ? client_init->timeout - 1000
                                        : client_init->timeout / 2;
-        c->ctx.max_buffer_size = H2O_SOCKET_INITIAL_INPUT_BUFFER_SIZE * 8;
+        c->ctx.max_buffer_size = 65536;
         memset(&c->ctx.http2, 0x00, sizeof(c->ctx.http2));
         if (client_init->http2_ratio != 0) {
             c->ctx.http2.max_concurrent_streams = 100;
